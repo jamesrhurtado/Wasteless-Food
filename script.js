58f6252c41b1845ad9954a21f4e44f825efa5e06
jrenzzo <renzo-hg03@hotmail.com>
2021-12-13 18:23:45 -0500

    navbar js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
